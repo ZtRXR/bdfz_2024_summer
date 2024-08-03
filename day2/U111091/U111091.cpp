4a90578dbe58d04680e26dc6bedbf5cbc2997424
Zengtudor <zengtudor@outlook.com>
2024-08-03 10:43:46 +0800

    update

--- day2/U111091/U111091.cpp
+++ day2/U111091/U111091.cpp
@@ -10,13 +10,13 @@ int l[MAX_N];
 int prefix_r[MAX_N];
 int r[MAX_N];
 
-void debug_array(int array[],size_t size,string name){
-    cout<<"\n"<<name<<" [";
-    for(int i=0;i<size;i++){
-        cout<<array[i]<<(i!=size-1?",":"");
-    }
-    cout<<"]\n";
-}
+// void debug_array(int array[],size_t size,string name){
+//     cout<<"\n"<<name<<" [";
+//     for(int i=0;i<size;i++){
+//         cout<<array[i]<<(i!=size-1?",":"");
+//     }
+//     cout<<"]\n";
+// }
 
 
 signed main(){
@@ -33,18 +33,21 @@ signed main(){
             cin>>x[i];
             if (max_house<=x[i])max_house=x[i];
         }
+        // cout<<"\nx[n]:"<<x[n]<<"\n";
         //RM
-        cout<<"\nmaxhouse:"<<max_house<<"\n";
+        // cout<<"\nmaxhouse:"<<max_house<<"\n";
         // max_house=x[n];
         //RM
-        debug_array(x, n, "x");
+        // debug_array(x+1, n, "x");
 
         string s;
         cin>>s;
 
         //RM
-        assert(s.size()==n-1);
-
+        // assert(s.size()==n-1);
+        for(int i=0;i<=max_house;i++){
+            r[i]=0;
+        }
         for(int i=1;i<=s.size();i++){
             if (s[i-1]=='1') {
                 l[i]=1;
@@ -58,15 +61,15 @@ signed main(){
         int cnt=0;
         for (int j=1; j<=k; j++) {
             //RM
-            cout<<"\nk:"<<j<<"\n";
-            debug_array(r, max_house, "r");
+            // cout<<"\nk:"<<j<<"\n";
+            // debug_array(r, max_house, "r");
 
             for(int i=1;i<=max_house;i++){
                 prefix_r[i]=prefix_r[i-1]+r[i];
             }
 
             //RM
-            debug_array(prefix_r, max_house, "prefix_r");
+            // debug_array(prefix_r, max_house, "prefix_r");
 
             int max_dir=INT_MIN;
             int max_num=INT_MIN;
@@ -78,10 +81,10 @@ signed main(){
             }
 
             //RM
-            debug_array(prefix_r, max_house, "prefix_r");
+            // debug_array(prefix_r, max_house, "prefix_r");
 
             //RM
-            cout<<"\nMAX_DIR:"<<max_dir<<"\n";
+            // cout<<"\nMAX_DIR:"<<max_dir<<"\n";
             for (int i=max_dir+1; i<=max_dir+m; i++) {
                 r[i]=0;
             }
@@ -90,11 +93,11 @@ signed main(){
                 cnt+=r[i];
             }
             //RM
-            cout<<"\ncnt:"<<cnt<<"\n";
+            // cout<<"\ncnt:"<<cnt<<"\n";
         }
         cout<<cnt<<"\n";
         //RM
-        cout<<"\nNEXT------------\n"<<endl;
+        // cout<<"\nNEXT------------\n"<<endl;
 
         /*
         cout<<"\nl:\n";
