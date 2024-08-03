03be638ba670c5956cb51e089d9895933eeeb574
Zengtudor <zengtudor@outlook.com>
2024-08-03 16:41:01 +0800

    update

--- day2/P7514/P7514.cpp
+++ day2/P7514/P7514.cpp
@@ -1,17 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+//DEBUG
+#define PRINT_VEC(vec) {\
+    cout<<"\n"<<#vec<<" [";\
+    for (size_t i=0; i<vec.size(); i++) {\
+        cout<<vec[i]<<(i!=vec.size()-1?",":"]\n");\
+    }\
+}
+
 #define int long long
+// #define p_vec(n) cout<<"n"<<endl;
+
+
 
 const int MAX_N = 1e6+6;
-int n,m,a[MAX_N],b[MAX_N];
+int n,m;
+
 
 signed main(){
     cin>>n>>m;
-    for (int i=1; i<=n; i++) {
+    vector<int> a(n),b(n);
+    for (int i=0; i<n; i++) {
         cin>>a[i];
     }
-    for (int i=1; i<=n; i++) {
+    PRINT_VEC(a)
+    
+    for (int i=0; i<n; i++) {
         cin>>b[i];
     }
-    
+    PRINT_VEC(b)
 }
