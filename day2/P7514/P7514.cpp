85339f6bbe805903794562b8d46fd619028c8bb5
Zengtudor <zengtudor@outlook.com>
2024-08-03 16:09:49 +0800

    update

---
+++ day2/P7514/P7514.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+const int MAX_N = 1e6+6;
+int n,m,a[MAX_N],b[MAX_N];
+
+signed main(){
+    cin>>n>>m;
+    for (int i=1; i<=n; i++) {
+        cin>>a[i];
+    }
+    for (int i=1; i<=n; i++) {
+        cin>>b[i];
+    }
+    
+}
