ca34cd3498974a6d780369a989dc3b805d27f55c
Zengtudor <zengtudor@outlook.com>
2024-08-03 11:19:23 +0800

    update

--- day2/U232856/U232856.cpp
+++ day2/U232856/U232856.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 
-int main(){
-    
+const int MAX_HW=1e9+5;
+int h,w,q1,q2;
+
+signed main(){
+    cin>>h>>w>>q1>>q2;
+    int grid[h][w];
+    for(int i=1;i<=q1;i++){
+        int r1,r2,c1,c2,x;
+        cin>>r1>>r2>>c1>>c2;
+        
+    }
+    for (int i=1 ;i<=q2;i++) {
+        int r1,r2,c1,c2;
+        cin>>r1>>r2>>c1>>c2;
+    }
 }
